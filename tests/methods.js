00caf914e97a48abab2b0a20fa4c6ac6a477e0af
Pete Corey <petecorey@gmail.com>
2015-08-01 11:48:25 -0700

    Hmm

--- tests/methods.js
+++ tests/methods.js
@@ -1,6 +1,19 @@
-describe('test', function() {
-  it('returns 123', function() {
-    //expect(visible()).to.equal(1337);
-    //console.log(Meteor);
-  })
-});
+var Fiber = require("fibers");
+
+//Meteor.bindEnvironment(function() {
+  describe('test', function() {
+    it('returns 123', function() {
+      console.log('test');
+      //expect(visible()).to.equal(1337);
+      //console.log(Meteor);
+      
+      // Meteor.methods({
+      //   test: function(a) {
+      //     return 123;
+      //   }
+      // });
+
+      //expect(Meteor.call('test', 1337)).to.equal(123);
+    })
+  });
+//});
